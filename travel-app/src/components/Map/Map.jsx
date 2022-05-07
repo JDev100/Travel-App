a0639ad30914dc72ca7e01663784cf90652bdb13
DaMan <jcazdev100@protonmail.com>
2022-05-07 16:21:59 -0700

    Rendered Map
    Added Api Key

--- travel-app/src/components/Map/Map.jsx
+++ travel-app/src/components/Map/Map.jsx
@@ -1,14 +1,32 @@
 import React from "react";
 import GoogleMapReact from 'google-map-react';
-import {Paper, Typography, userMediaQuery} from '@material-ui/core';
+import {Paper, Typography, useMediaQuery} from '@material-ui/core';
 import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
 import Rating from '@material-ui/lab';
 
-
+import makeStyles from './styles';
 
 const Map = () => {
+    const classes = makeStyles();
+    const isMobile = useMediaQuery('(min-width:600px)')
+    
+    const coordinates = {lat: 0, lng: 0}
     return (
-        <h1>Map</h1>
+        <div className={classes.mapContainer}>
+            <GoogleMapReact
+                bootstrapURLKeys={{key:'AIzaSyDWb09vqqpZ4_7apMkSn4pOHSxg5FPq5H0'}}
+                defaultCenter={coordinates}
+                center={coordinates}
+                defaultZoom={14}
+                margin={[50,50,50,50]}
+                options = {''}
+                onChange={''}
+                onChildClick={''}
+            >
+
+            </GoogleMapReact>
+        </div>
+
     );
 }
 
